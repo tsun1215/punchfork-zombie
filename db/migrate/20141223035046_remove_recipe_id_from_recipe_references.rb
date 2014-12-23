42d231a914b89255bf7d162fde9b6f003fa4d63b
Terence Sun <tsun1215@gmail.com>
2014-12-22 19:59:23 -0800

    Added back recipe_id for recipe_refs

--- db/migrate/20141223035046_remove_recipe_id_from_recipe_references.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRecipeIdFromRecipeReferences < ActiveRecord::Migration
-  def change
-    remove_column :recipe_references, :recipe_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223035046) do
+ActiveRecord::Schema.define(version: 20141223034817) do
 
   create_table "recipe_references", force: :cascade do |t|
     t.string   "name"
+    t.integer  "recipe_id"
     t.string   "external"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
