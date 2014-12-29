c6aa675c8b93c09bf421a6a16b8030a0fa74297c
Terence Sun <tsun1215@gmail.com>
2014-12-29 11:31:27 -0800

    Added 2-way one-to-one relation between recipe & refs

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,13 @@ end
 100.times do |n|
   RecipeReference.create!(name: "Recipe #{n}", external: "http://reddit.com/r/food", user_id: ((n%10)+1))
 end
+
+50.times do |n|
+  r = Recipe.create!(description: "Description for internal recipe #{n}", ingredients: {"Ing#{n}" => "#{n} cups"}, instructions: "1. Get #{n} cups of Ing#{n} and stir-fry")
+  ref = RecipeReference.create(name: "Internal Recipe #{n}", recipe_id: r.id)
+  ref.recipe_id = r.id
+  ref.user_id = (n%10) + 1
+  ref.save
+  r.recipe_reference_id = ref.id
+  r.save
+end
